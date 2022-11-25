1524c127bb324d6426fb1da427a5b3c8d8faa296
soichiroooo <slamdunkducksky@gmail.com>
2022-11-25 16:41:49 +0900

    itemsコントローラ内の記述を簡潔なものに修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    if current_user.id != @item.user_id
       redirect_to action: :index
     end
   end
