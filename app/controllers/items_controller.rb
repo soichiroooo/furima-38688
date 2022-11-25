0476567691865ab0cece9fb493a2ef3b0e3ea5ea
soichiroooo <slamdunkducksky@gmail.com>
2022-11-25 17:12:41 +0900

    destroyアクションを簡潔な記述へ変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,10 +38,8 @@ class ItemsController < ApplicationController
   def destroy
     if current_user.id == @item.user_id
       @item.destroy
-      redirect_to action: :index
-    else
-      redirect_to action: :index
     end
+    redirect_to action: :index
   end
 
   private
