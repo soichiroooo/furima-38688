4c19b22ddaf169a188cd118e65ca85f1e5af383f
soichiroooo <slamdunkducksky@gmail.com>
2022-11-29 20:56:36 +0900

    クレジットカード機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,22 +1,26 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
-    @purchase_address = PurchaseAddress.new
-  end
-
-  def new
     @purchase_address = PurchaseAddress.new
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
     if @purchase_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
       @purchase_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
@@ -24,8 +28,17 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, 
-                                             :address_line, :building, :phone_number, 
-                                             :purchase_id, :user_id, 
-                                             :item_id).merge(user_id: current_user.id)
+                                             :address_line, :building, :phone_number, :purchase_id
+                                            ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id || Purchase.where(item_id: @item.id).exists?
+      redirect_to root_path
+    end
   end
 end
