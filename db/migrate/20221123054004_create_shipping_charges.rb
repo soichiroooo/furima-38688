5641bfbc88c866e199e5b350e1e9f59cef8966ae
soichiroooo <slamdunkducksky@gmail.com>
2022-12-02 17:37:52 +0900

    不要なファイルを削除

--- db/migrate/20221123054004_create_shipping_charges.rb
+++
@@ -1,8 +0,0 @@
-class CreateShippingCharges < ActiveRecord::Migration[6.0]
-  def change
-    create_table :shipping_charges do |t|
-      t.integer :shipping_charge_id     , null: false
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20221123054032_create_shipping_dates.rb
+++
@@ -1,8 +0,0 @@
-class CreateShippingDates < ActiveRecord::Migration[6.0]
-  def change
-    create_table :shipping_dates do |t|
-      t.integer :shipping_date_id     , null: false
-      t.timestamps
-    end
-  end
-end
